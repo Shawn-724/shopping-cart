371f931276273b9aeaae4a6770f1f8e7420b2850
Shawn-724 <youxiong.shawn.ye@gmail.com>
2024-12-29 15:45:31 -0600

    fixed radio and delivery date

--- src/components/FulfilmentOption.js
+++ src/components/FulfilmentOption.js
@@ -17,7 +17,7 @@ const FulfilmentOption = ({ quantity, handleFulfilmentOption }) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [selectedFulfilmentOption]);
 
-  const checkIfStock = () => {
+  const checkInStock = () => {
     if (quantity <= fulfillmentData.inStockPickUpAvailableQty) {
       return <Text style={{ color: "green" }}>&#10003; In Stock</Text>;
     } else {
@@ -26,7 +26,11 @@ const FulfilmentOption = ({ quantity, handleFulfilmentOption }) => {
   };
 
   return (
-    <Radio.Group onChange={onChange} value={selectedFulfilmentOption}>
+    <Radio.Group
+      onChange={onChange}
+      value={selectedFulfilmentOption}
+      className="antd-radio"
+    >
       <div className="Fulfilment-option">
         <Space
           className="Fulfilment-option"
@@ -37,7 +41,7 @@ const FulfilmentOption = ({ quantity, handleFulfilmentOption }) => {
         >
           <Radio value={101}>Free In-Store or Curbside Pick Up</Radio>
           <Text style={{ paddingLeft: "25px" }}>
-            {checkIfStock()} {"| "}
+            {checkInStock()} {"| "}
             <span style={{ textDecoration: "underline" }}>
               {storeData.storeAddress.toUpperCase()}
             </span>
@@ -52,7 +56,10 @@ const FulfilmentOption = ({ quantity, handleFulfilmentOption }) => {
           <Radio value={102}>Home Delivery</Radio>
           <Text style={{ paddingLeft: "40px" }}>Standard Delivery </Text>
           <Text style={{ paddingLeft: "40px" }}>
-            Est. Delivery <span style={{ color: "green" }}>Apr. 08-11</span>
+            Est. Delivery{" "}
+            <span style={{ color: "green" }}>
+              {fulfillmentData.deliveryData.substring(15, 26)}
+            </span>
           </Text>
         </Space>
       </div>
